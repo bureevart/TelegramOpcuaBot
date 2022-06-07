0e81f4a6bead995eb072b7be03d10e279635054c
bureevar <bureevar@LAPTOP-V3C1RBOS>
2022-06-07 14:34:51 +0500

    Добавьте файлы проекта.

---
+++ TelegramObcuaBot/BotCommandManager.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Exceptions;
+
+namespace TelegramObcuaBot
+{
+    internal class BotCommandManager
+    {
+        Message message;
+        internal ITelegramBotClient botClient;
+        public string[] BotCommands;
+        static bool isTryingLoggedIn = false;
+        static bool isLoggedIn = false;
+        private string[] _loginData;
+
+        internal BotCommandManager(Message message, ITelegramBotClient botClient, string[] BotCommands, String[] _loginData)
+        {
+            this.message = message;
+            this.botClient = botClient;
+            this.BotCommands = BotCommands;
+            this._loginData = _loginData;
+        }
+        internal async Task Manager()
+        {
+            switch(message.Text)
+            {
+                case "/start":
+                    await StartMessageAsync();
+
+                    return;
+                case "/help":
+                    await HelpMessageAsync();
+
+                    return;
+                case "/login":
+                    await LoginMessageAsync();
+
+                    return;
+                case "/check":
+                    await CheckMessageAsync();
+
+                    return;
+                case "/logout":
+                    await LogoutMessageAsync();
+
+                    return;
+                default:
+                    if (isTryingLoggedIn)
+                    {
+                        await EnterLoginData();
+
+                        return;
+                    }
+                    await botClient.SendTextMessageAsync(message.Chat, "Ничего не понял, повтори ввод или введи /help для для того чтобы посмотреть на мои возможности! ");
+
+                    return;
+            }
+        }
+        async Task StartMessageAsync()
+        {
+            await botClient.SendTextMessageAsync(message.Chat, "Добро пожаловать на борт, введи /help для для того чтобы посмотреть на мои возможности!");
+        }
+
+        async Task HelpMessageAsync()
+        {
+            var info = "Список команд: \n";
+
+            for (int i = 0; i < BotCommands.Length; i++)
+            {
+                info += BotCommands[i] + "\n";
+            }
+            await botClient.SendTextMessageAsync(message.Chat, info);
+        }
+
+        async Task LoginMessageAsync()
+        {
+            await botClient.SendTextMessageAsync(message.Chat, "Введите через пробел IP сервера, свой логин и пароль: ");
+            //login-code
+            isTryingLoggedIn = true;
+        }
+
+        async Task CheckMessageAsync()
+        {
+            if (!isLoggedIn)
+            {
+                await botClient.SendTextMessageAsync(message.Chat, "Сперва нужно залогинится!");
+                return;
+            }
+            //code
+            await botClient.SendTextMessageAsync(message.Chat, "data (заглушка)");
+        }
+
+        async Task LogoutMessageAsync()
+        {
+            if (isLoggedIn)
+            {
+                isLoggedIn = false;
+                await botClient.SendTextMessageAsync(message.Chat, "Вы вышли из аккаунта!");
+            }
+            else
+            {
+                await botClient.SendTextMessageAsync(message.Chat, "Вы не залогинены, вам не нужно выходить из аккаунта!");
+            }
+        }
+
+        async Task EnterLoginData()
+        {
+            Console.WriteLine(message.Text);
+
+            _loginData = message.Text.Split(" ");
+            //вывод информации в консоль
+            Console.WriteLine("Данные для входа в аккаунт: ");
+            for (int i = 0; i < _loginData.Length; i++)
+            {
+                Console.WriteLine(_loginData[i]);
+            }
+
+            //проверка на правильность введенных данных (доделать)
+            if (_loginData.Length == 3)
+            {
+                await botClient.SendTextMessageAsync(message.Chat, "Вы залогинились!");
+                isLoggedIn = true;
+            }
+            else
+            {
+                await botClient.SendTextMessageAsync(message.Chat, "Введены неверные данные!");
+            }
+
+            isTryingLoggedIn = false;
+        }
+
+    }
+}
---
+++ TelegramObcuaBot/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Exceptions;
+using TelegramObcuaBot;
+
+namespace TelegramBotExperiments
+{
+
+    public class Program
+    {
+        public static string[] BotCommands =
+        {
+            "/start - запуск бота",
+            "/help - справка по командам",
+            "/login - залогинится на сервере",
+            "/check - считать показатели",
+            "/logout - выйти из сети"
+        };
+
+        private static string[] _loginData; 
+        private static string _token = "5580137395:AAF9O0KRHKY1BLRa7mgws4psH816qqpyBUI";
+        static BotCommandManager _botCommandManager;
+
+        private static ITelegramBotClient _bot;
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            // Некоторые действия
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
+            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            {
+                var message = update.Message;
+                _botCommandManager = new BotCommandManager(message, botClient, BotCommands, _loginData);
+                await _botCommandManager.Manager();
+                
+            }
+           
+        }
+
+        public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            // Некоторые действия
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+        }
+
+
+        static void Main(string[] args)
+        {
+            _bot = new TelegramBotClient(_token);
+            Console.WriteLine("Запущен бот " + _bot.GetMeAsync().Result.FirstName);
+
+            var cts = new CancellationTokenSource();
+            var cancellationToken = cts.Token;
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { }, // receive all update types
+            };
+            _bot.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                receiverOptions,
+                cancellationToken
+            );
+
+            Console.ReadLine();
+        }
+    }
+}
